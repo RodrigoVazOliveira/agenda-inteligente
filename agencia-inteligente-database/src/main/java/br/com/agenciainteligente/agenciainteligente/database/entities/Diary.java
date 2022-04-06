f8249aa49f75be45908f392e86d0ca4781fc9a79
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-04-06 16:53:21 -0300

    Created entity diary

---
+++ agencia-inteligente-database/src/main/java/br/com/agenciainteligente/agenciainteligente/database/entities/Diary.java
@@ -0,0 +1,85 @@
+package br.com.agenciainteligente.agenciainteligente.database.entities;
+
+import br.com.agenciainteligente.agenciainteligente.database.helpers.DeserializeToString;
+
+import java.time.LocalDateTime;
+
+public class Diary {
+    private Long id;
+    private Profile profile;
+    private LocalDateTime dateTime;
+    private String subject;
+    private String description;
+    private String comments;
+    private Place place;
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return DeserializeToString.getString(this);
+    }
+}
