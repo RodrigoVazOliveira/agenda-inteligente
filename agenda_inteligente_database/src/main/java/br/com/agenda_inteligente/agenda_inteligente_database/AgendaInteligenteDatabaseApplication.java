4afa940ca1755a400e3a61d2734e2b8e4d16da9e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-04-07 07:31:01 -0300

    Create project with name correct

---
+++ agenda_inteligente_database/src/main/java/br/com/agenda_inteligente/agenda_inteligente_database/AgendaInteligenteDatabaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenda_inteligente.agenda_inteligente_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaInteligenteDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaInteligenteDatabaseApplication.class, args);
+	}
+
+}
---
+++ agenda_inteligente_database/src/test/java/br/com/agenda_inteligente/agenda_inteligente_database/AgendaInteligenteDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agenda_inteligente.agenda_inteligente_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaInteligenteDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
